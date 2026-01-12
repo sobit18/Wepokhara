7b4710950670ac01dff6719cf80e9b72c0b36151
sobit18 <sobitshakya5@gmail.com>
2026-01-12 13:12:40 +0545

    [create advertisement model: created]

---
+++ src/models/Advertisement.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const advertisementSchema = new mongoose.Schema(
+  {
+    photo: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Advertisement", advertisementSchema);
