213be30234cf9f657f31f184b4190e339c9acf9d
sobit18 <sobitshakya5@gmail.com>
2026-01-20 15:42:10 +0545

    [create donation model: created]

---
+++ src/models/Donation.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const donationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Donation", donationSchema);
